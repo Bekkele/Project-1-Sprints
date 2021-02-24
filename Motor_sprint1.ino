707d728edc6873362202cf80141ecc4a7c171bad
Bekklele <73904870+Bekklele@users.noreply.github.com>
2021-02-24 12:16:04 -0500

    Update Motor_sprint1.ino

--- Motor_sprint1.ino
+++ Motor_sprint1.ino
@@ -1,6 +1,14 @@
-/* edited Sweep
-*/
+/* Sweep
+ *  
+ *  modified by Bekkele Hamilton
+ *  
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
 
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Sweep
+*/
 #include <Servo.h>
 
 Servo myservo;  // create servo object to control a servo
